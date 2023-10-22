855f0ba64d322739625899cc1d0bbb8d64677f71
CGelet <iluvdrpepper03@gmail.com>
2023-10-21 20:34:53 -0400

    Program Testing

--- StateSpaceLoggerV.1.ino
+++ StateSpaceLoggerV.1.ino
@@ -1,7 +1,10 @@
 #include <Wire.h>
 #include "SparkFunBME280.h"
 #include "SparkFunLSM6DSO.h"
+#include "MLX90393.h"
 
+MLX90393 mlx;
+MLX90393::txyz data;
 LSM6DSO myIMU;    // Default constructor is I2C, addr 0x6B
 BME280 mySensorA; // Uses default I2C address 0x77
 
@@ -22,10 +25,15 @@ void setup()
 
   if (myIMU.initialize(BASIC_SETTINGS))
     Serial.println("Loaded Settings.");
+  
+  mlx.begin(); //Assumes I2C jumpers are GND. No DRDY pin used.
+  mlx.setOverSampling(0);
+  mlx.setDigitalFiltering(0);
 }
 
 void loop()
 {
+  /*
   Serial.print("HumidityA: ");
   Serial.print(mySensorA.readFloatHumidity(), 0);
 
@@ -37,7 +45,7 @@ void loop()
   Serial.print(mySensorA.readTempF(), 2);
 
   Serial.println();
-  delay(10);
+  delay(500);
   // Get all parameters
   Serial.print("\nAccelerometer:\n");
   Serial.print(" X = ");
@@ -54,7 +62,17 @@ void loop()
   Serial.println(myIMU.readFloatGyroY(), 3);
   Serial.print(" Z = ");
   Serial.println(myIMU.readFloatGyroZ(), 3);
-
+  */
   Serial.println();
   delay(500);
+  
+  mlx.readData(data); //Read the values from the sensor
+  Serial.print("magX[");
+  Serial.print(data.x);
+  Serial.print("] magY[");
+  Serial.print(data.y);
+  Serial.print("] magZ[");
+  Serial.print(data.z);
+  delay(500);
+  Serial.println();
 }
