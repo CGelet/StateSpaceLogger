10e6c5d0aeacef7ef933f569cbe0d1c10748a7a5
CGelet <iluvdrpepper03@gmail.com>
2023-10-28 16:25:21 -0400

    DMP_Implementation

--- StateSpaceLogger.ino
+++ StateSpaceLogger.ino
@@ -1,3 +1,8 @@
+#include "electricui.h"
+#include <eui_config.h>
+#include <eui_macro.h>
+#include <eui_types.h>
+#include <eui_utilities.h>
 #include <Wire.h>
 #include "src/SparkFunBME280.h"
 #include "src/ICM_20948.h"
@@ -7,13 +12,31 @@ ICM_20948_I2C myICM; // Uses default I2C address 0x69
 #define WIRE_PORT Wire
 #define AD0_VAL 1
 
+// EUI Initialization
+double q1;
+double q2;
+double q3;
+eui_message_t tracked_variables[] =
+{
+  EUI_INT32("Q1", q1),
+  EUI_INT32("Q2", q2),
+  EUI_INT32("Q3", q3),
+};
+eui_interface_t serial_comms = EUI_INTERFACE( &serial_write ); 
+
 void setup()
 {
+  // Serial and EUI
   Serial.begin(115200);
   while (!Serial)
   {
   };
 
+  eui_setup_interface( &serial_comms );
+  EUI_TRACK( tracked_variables );
+  eui_setup_identifier( "MRT SSL", 5 );
+
+  // Others
   Wire.begin();
   ES_Sens.beginI2C();
   myICM.begin(WIRE_PORT, AD0_VAL);
@@ -21,11 +44,11 @@ void setup()
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
 
-bool initialized = false;
+  bool initialized = false;
 #ifdef USE_SPI
-    myICM.begin(CS_PIN, SPI_PORT);
+  myICM.begin(CS_PIN, SPI_PORT);
 #else
-    myICM.begin(WIRE_PORT, AD0_VAL);
+  myICM.begin(WIRE_PORT, AD0_VAL);
 #endif
 
   Serial.print(F("Initialization of the sensor returned: "));
@@ -40,27 +63,55 @@ bool initialized = false;
     Serial.println("Success!");
     initialized = true;
   }
- //success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok); 
+  // DMP Enabling
+  bool success = true;
+  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  // Enable the FIFO
+  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
+
+  // Enable the DMP
+  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
+
+  // Reset DMP
+  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
+
+  // Reset FIFO
+  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
+  if (success)
+  {
+#ifndef QUAT_ANIMATION
+    Serial.println(F("DMP enabled!"));
+#endif
+  }
+  else
+  {
+    Serial.println(F("Enable DMP failed!"));
+    while (1)
+      ; // Do nothing more
+  }
 }
 
 void loop()
 {
-
+  serial_rx_handler();
   // es_sensor_readings();
   // Serial.println();
   // delay(1000);
+  /*
   if (myICM.dataReady())
   {
-  myICM.getAGMT();
-  icm_20948_readings(&myICM);
-  delay(30);
+    myICM.getAGMT();
+    icm_20948_readings(&myICM);
+    delay(30);
   }
   else
   {
     Serial.println("Data not ready!");
     delay(500);
-  }
-
+  }*/
+  dmp_readings();
 }
 
 void es_sensor_readings()
@@ -194,3 +245,59 @@ void icm_20948_readings(ICM_20948_I2C *sensor)
   Serial.println();
 }
 
+void dmp_readings()
+{
+  icm_20948_DMP_data_t data;
+  myICM.readDMPdataFromFIFO(&data);
+  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
+  {
+    if ((data.header & DMP_header_bitmap_Quat9) > 0) // We have asked for orientation data so we should receive Quat9
+    {
+      double q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
+#ifndef QUAT_ANIMATION
+      Serial.print(F("Q1:"));
+      Serial.print(q1, 3);
+      Serial.print(F(" Q2:"));
+      Serial.print(q2, 3);
+      Serial.print(F(" Q3:"));
+      Serial.print(q3, 3);
+      Serial.print(F(" Accuracy:"));
+      Serial.println(data.Quat9.Data.Accuracy);
+#else
+      // Output the Quaternion data in the format expected by ZaneL's Node.js Quaternion animation tool
+      Serial.print(F("{\"quat_w\":"));
+      Serial.print(q0, 3);
+      Serial.print(F(", \"quat_x\":"));
+      Serial.print(q1, 3);
+      Serial.print(F(", \"quat_y\":"));
+      Serial.print(q2, 3);
+      Serial.print(F(", \"quat_z\":"));
+      Serial.print(q3, 3);
+      Serial.println(F("}"));
+#endif
+    }
+  }
+
+  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
+  {
+    delay(10);
+  }
+}
+
+// EUI Functions
+
+void serial_rx_handler()
+{
+  // While we have data, we will pass those bytes to the ElectricUI parser
+  while( Serial.available() > 0 )  
+  {  
+    eui_parse( Serial.read(), &serial_comms );  // Ingest a byte
+  }
+}
+void serial_write( uint8_t *data, uint16_t len )
+{
+  Serial.write( data, len ); //output on the main serial port
+}
