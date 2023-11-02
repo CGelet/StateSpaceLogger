935c69ef5e73432f1438931c44938de8e9db4c4b
CGelet <iluvdrpepper03@gmail.com>
2023-11-02 09:02:50 -0400

    Rebuild

--- StateSpaceLogger.ino
+++ StateSpaceLogger.ino
@@ -1,319 +1,204 @@
-#include "electricui.h"
-#include <eui_config.h>
-#include <eui_macro.h>
-#include <eui_types.h>
-#include <eui_utilities.h>
+// Initializing Laibraries
 #include <Wire.h>
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include "src/SparkFunBME280.h"
 #include "src/ICM_20948.h"
+// Will need UV Sensor Setup
 
-BME280 ES_Sens;      // Uses default I2C address 0x77
-ICM_20948_I2C myICM; // Uses default I2C address 0x69
-#define WIRE_PORT Wire
+// Analog Pin Setup
+// GUVAS12SD uv(A0);
+
+// Digital Pin Setup
+BME280 ES_Sens;         // Uses default I2C address 0x77
+ICM_20948_I2C IMU_Sens; // Uses default I2C address 0x69
+OpenLog LOGGER;         // Uses default I2C address 0x32
 #define AD0_VAL 1
 
-// EUI Initialization
-eui_interface_t serial_comms = EUI_INTERFACE( &serial_write ); // Create an interface for Serial
-double q1;
-double q2;
-double q3;
-double q0;
-eui_message_t tracked_variables[] =
-{
-  EUI_DOUBLE("Q1", q1),
-  EUI_DOUBLE("Q2", q2),
-  EUI_DOUBLE("Q3", q3),
-  EUI_DOUBLE("Q0", q0),
-};
-
+// Analog Pin Setup
+int SyncPin = A0;
+int UV1 = A2;
+// Variable Setup
+int Iteration = 0;
+int KeepOn;
 void setup()
 {
-  // Serial and EUI
-  Serial.begin(115200);
-  while (!Serial)
-  {
-  };
+    Serial.begin(9600); // Setting Baud Rate to 115200
+    while (!Serial)
+    {
+    }; // Waiting for Serial to register
 
-  eui_setup_interface( &serial_comms );
-  EUI_TRACK( tracked_variables );
-  eui_setup_identifier( "MRT SSL", 13 );
+    // Sensor / I2C Setup
+    Wire.begin();          // Starting I2C
+    Wire.setClock(400000); // Setting I2C Clock Rate to 400kHz
+    ES_Sens.beginI2C();    // Starting BME280
+    LOGGER.begin();        // Starting OpenLog
+    
+    // Output Setup
+    pinMode(SyncPin,OUTPUT);
 
-  // Others
-  Wire.begin();
-  ES_Sens.beginI2C();
-  myICM.begin(WIRE_PORT, AD0_VAL);
+    // IMU Setup and Testing
+    IMU_Sens.enableDebugging();
+    bool IMU_Init = false;
 
-  WIRE_PORT.begin();
-  WIRE_PORT.setClock(400000);
-
-  bool initialized = false;
-#ifdef USE_SPI
-  myICM.begin(CS_PIN, SPI_PORT);
-#else
-  myICM.begin(WIRE_PORT, AD0_VAL);
-#endif
-
-  Serial.print(F("Initialization of the sensor returned: "));
-  Serial.println(myICM.statusString());
-  if (myICM.status != ICM_20948_Stat_Ok)
-  {
-    Serial.println("Trying again...");
-    delay(500);
-  }
-  else
-  {
-    Serial.println("Success!");
-    initialized = true;
-  }
-  // DMP Enabling
-  bool success = true;
-  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
-  // Enable the FIFO
-  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
-
-  // Enable the DMP
-  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
-
-  // Reset DMP
-  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
-
-  // Reset FIFO
-  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
-  if (success)
-  {
-#ifndef QUAT_ANIMATION
-    Serial.println(F("DMP enabled!"));
-#endif
-  }
-  else
-  {
-    Serial.println(F("Enable DMP failed!"));
-    while (1)
-      ; // Do nothing more
-  }
+    while (!IMU_Init)
+    {
+        IMU_Sens.begin(Wire, AD0_VAL); // Starting ICM20948
+        Serial.print(F("Initialization of the sensor returned: "));
+        Serial.println(IMU_Sens.statusString());
+        if (IMU_Sens.status != ICM_20948_Stat_Ok)
+        {
+            Serial.println(F("Trying again..."));
+            delay(500);
+        }
+        else
+        {
+            IMU_Init = true;
+        }
+    }
+    Serial.println("IMU Sucessfully Connected!");
+    /* Future DMP Setup
+    // DMP Setup and Testing
+    bool DMP_Init = true;
+    DMP_Init =&
+    */
+   // Sensor Range Setup
 }
-
 void loop()
 {
-  int old_time = 0;
-  int new_time = millis();
-  int dt = new_time - old_time;
-  serial_rx_handler();
-  // es_sensor_readings();
-  // Serial.println();
-  // delay(1000);
-  
-  if (myICM.dataReady())
-  {
-    myICM.getAGMT();
-    icm_20948_readings(&myICM);
-    delay(30);
-  }
-  else
-  {
-    Serial.println("Data not ready!");
-    delay(500);
-  }
-  serial_rx_handler();
-  dmp_readings();
-}
-// BME Functions
-void es_sensor_readings()
-{
-  Serial.print("Temperature: ");
-  Serial.print(ES_Sens.readTempC());
-  Serial.println(" C");
-
-  Serial.print("Humidity: ");
-  Serial.print(ES_Sens.readFloatHumidity());
-  Serial.println(" %");
-
-  Serial.print("Pressure: ");
-  Serial.print(ES_Sens.readFloatPressure());
-  Serial.println(" Pa");
-
-  Serial.print("Altitude: ");
-  Serial.print(ES_Sens.readFloatAltitudeMeters());
-  Serial.println(" m");
-
-  Serial.println();
-}
-
-// Print Functions
-
-void printPaddedInt16b(int16_t val)
-{
-  if (val > 0)
-  {
-    Serial.print(" ");
-    if (val < 10000)
-    {
-      Serial.print("0");
+    Iteration = Iteration + 1;
+    //IMU_Sens.getAGMT(); // Getting Accelerometer, Gyroscope, Magnetometer, and Temperature Readings
+    //AGMT_Readings(&IMU_Sens); // Calling AGMT Function to read values
+    UVSensors();
+    delay(1000);
+    if (Iteration == 20) {
+        analogWrite(SyncPin,255);
+        Iteration = 0;
+        KeepOn = random(3,8);
     }
-    if (val < 1000)
-    {
-      Serial.print("0");
+    if (Iteration == KeepOn){
+        analogWrite(SyncPin,0);
     }
-    if (val < 100)
-    {
-      Serial.print("0");
-    }
-    if (val < 10)
-    {
-      Serial.print("0");
-    }
-  }
-  else
-  {
-    Serial.print("-");
-    if (abs(val) < 10000)
-    {
-      Serial.print("0");
-    }
-    if (abs(val) < 1000)
-    {
-      Serial.print("0");
-    }
-    if (abs(val) < 100)
-    {
-      Serial.print("0");
-    }
-    if (abs(val) < 10)
-    {
-      Serial.print("0");
-    }
-  }
-  Serial.print(abs(val));
+    //Serial.println(Iteration);
 }
 
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
 {
-  float aval = abs(val);
-  if (val < 0)
-  {
-    Serial.print("-");
-  }
-  else
-  {
-    Serial.print(" ");
-  }
-  for (uint8_t indi = 0; indi < leading; indi++)
-  {
-    uint32_t tenpow = 0;
-    if (indi < (leading - 1))
+    float aval = abs(val);
+    if (val < 0)
     {
-      tenpow = 1;
-    }
-    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
-    {
-      tenpow *= 10;
-    }
-    if (aval < tenpow)
-    {
-      Serial.print("0");
+        Serial.print("-");
     }
     else
     {
-      break;
+        Serial.print(" ");
     }
-  }
-  if (val < 0)
-  {
-    Serial.print(-val, decimals);
-  }
-  else
-  {
-    Serial.print(val, decimals);
-  }
-}
-
-// ICM Functions
-
-void icm_20948_readings(ICM_20948_I2C *sensor)
-{
-  Serial.print("Scaled. Acc (mg) [ ");
-  printFormattedFloat(sensor->accX(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->accY(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->accZ(), 5, 2);
-  Serial.print(" ], Gyr (DPS) [ ");
-  printFormattedFloat(sensor->gyrX(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->gyrY(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->gyrZ(), 5, 2);
-  Serial.print(" ], Mag (uT) [ ");
-  printFormattedFloat(sensor->magX(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->magY(), 5, 2);
-  Serial.print(", ");
-  printFormattedFloat(sensor->magZ(), 5, 2);
-  Serial.print(" ]");
-  Serial.println();
-}
-
-// DMP Functions
-
-void dmp_readings()
-{
-  icm_20948_DMP_data_t data;
-  myICM.readDMPdataFromFIFO(&data);
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
-  {
-    if ((data.header & DMP_header_bitmap_Quat9) > 0) // We have asked for orientation data so we should receive Quat9
+    for (uint8_t indi = 0; indi < leading; indi++)
     {
-      q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
-      q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
-#ifndef QUAT_ANIMATION
-
-     Serial.print(F("Q0:"));
-      Serial.print(q0, 3);
-     Serial.print(F("Q1:"));
-      Serial.print(q1, 3);
-      Serial.print(F(" Q2:"));
-      Serial.print(q2, 3);
-      Serial.print(F(" Q3:"));
-      Serial.print(q3, 3);
-      Serial.print(F(" Accuracy:"));
-      Serial.println(data.Quat9.Data.Accuracy);
-#else
-      // Output the Quaternion data in the format expected by ZaneL's Node.js Quaternion animation tool
-      Serial.print(F("{\"quat_w\":"));
-      Serial.print(q0, 3);
-      Serial.print(F(", \"quat_x\":"));
-      Serial.print(q1, 3);
-      Serial.print(F(", \"quat_y\":"));
-      Serial.print(q2, 3);
-      Serial.print(F(", \"quat_z\":"));
-      Serial.print(q3, 3);
-      Serial.println(F("}"));
-#endif
+        uint32_t tenpow = 0;
+        if (indi < (leading - 1))
+        {
+            tenpow = 1;
+        }
+        for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+        {
+            tenpow *= 10;
+        }
+        if (aval < tenpow)
+        {
+            Serial.print("0");
+        }
+        else
+        {
+            break;
+        }
     }
-  }
-
-  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
-  {
-    delay(10);
-  }
+    if (val < 0)
+    {
+        Serial.print(-val, decimals);
+    }
+    else
+    {
+        Serial.print(val, decimals);
+    }
 }
 
-// EUI Functions
-
-void serial_rx_handler()
+void AGMT_Readings(ICM_20948_I2C *sensor)
 {
-  // While we have data, we will pass those bytes to the ElectricUI parser
-  while( Serial.available() > 0 )  
-  {  
-    eui_parse( Serial.read(), &serial_comms );  // Ingest a byte
-  }
+    if (IMU_Sens.dataReady())
+    {
+        // Accelerometer, Gyroscope, Magnetometer, and Temperature Readings
+        float Accel[3];
+        float Gyro[3];
+        float Mag[3];
+        float Temp;
+        Accel[0] = IMU_Sens.accX();
+        Accel[1] = IMU_Sens.accY();
+        Accel[2] = IMU_Sens.accZ();
+        Gyro[0] = IMU_Sens.gyrX();
+        Gyro[1] = IMU_Sens.gyrY();
+        Gyro[2] = IMU_Sens.gyrZ();
+        Mag[0] = IMU_Sens.magX();
+        Mag[1] = IMU_Sens.magY();
+        Mag[2] = IMU_Sens.magZ();
+
+        /*Serial.print("Accel: ");
+        Serial.print(Accel[0]);
+        Serial.print(", ");
+        Serial.print(Accel[1]);
+        Serial.print(", ");
+        Serial.print(Accel[2]);
+        Serial.print(" | Gyro: ");
+        Serial.print(Gyro[0]);
+        Serial.print(", ");
+        Serial.print(Gyro[1]);
+        Serial.print(", ");
+        Serial.print(Gyro[2]);
+        Serial.print(" | Mag: ");
+        printFormattedFloat(Mag[0], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Mag[1], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Mag[2], 5, 2);
+        delay(30);*/
+        Serial.print("Scaled. Acc (mg) [ ");
+        printFormattedFloat(Accel[0], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Accel[1], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Accel[2], 5, 2);
+        Serial.print(" ], Gyr (DPS) [ ");
+        printFormattedFloat(Gyro[0], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Gyro[1], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Gyro[2], 5, 2);
+        Serial.print(" ], Mag (uT) [ ");
+        printFormattedFloat(Mag[0], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Mag[1], 5, 2);
+        Serial.print(", ");
+        printFormattedFloat(Mag[2], 5, 2);
+        Serial.print(" ]");
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("Data not Ready");
+        delay(500);
+    }
 }
 
-void serial_write( uint8_t *data,  uint16_t len )
-{
-  Serial.write( data, len ); //output on the main serial port
+void UVSensors(){
+    int sensorValue;
+    long sum1;
+    for (int i =0; i<100; i++){
+        sensorValue = analogRead(A2);
+        //Serial.println(sensorValue);
+        sum1 = sensorValue+sum1;
+        delay(2);
+    }
+    long meanVal = sum1/1024;  // get mean value
+    Serial.print("The current UV index is:");
+    Serial.print((meanVal*1000/4.3-83)/21);// get a detailed calculating expression for UV index in schematic files.
+    Serial.print("\n");
 }
